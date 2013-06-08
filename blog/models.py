7dd45b2ab513963037b48065d63423bdf92cbd54
josedeweb <josealvarez712@gmail.com>
2013-06-08 12:52:32 -0500

    cambios ruta y fecha

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
-	creado = models.DateTimeField(auto_now_add=True, db_index=True)
-	titulo = models.CharField(blank=True,max_length=255)
+	create = models.DateTimeField(auto_now_add=True, db_index=True)
+	title = models.CharField(blank=True,max_length=255)
 	img = models.ImageField(blank=True,upload_to='media/')
-	contenido = models.TextField()
+	content = models.TextField()
 
 	def __unicode__(self):
-		return self.titulo
+		return self.title
