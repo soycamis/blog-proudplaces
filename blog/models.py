b90e2a15854dbe9cd51ee347fc3a87740842ed19
josedeweb <josealvarez712@gmail.com>
2013-06-08 12:59:22 -0500

    ajustada la fecha y los nombres

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-	create = models.DateTimeField(auto_now_add=True, db_index=True)
+	created = models.DateTimeField(auto_now_add=True, db_index=True)
 	title = models.CharField(blank=True,max_length=255)
 	img = models.ImageField(blank=True,upload_to='media/')
 	content = models.TextField()
