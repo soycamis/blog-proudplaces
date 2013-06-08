a63945f681fb4869ac813c8d35248937ff379abc
josedeweb <josealvarez712@gmail.com>
2013-06-08 11:20:05 -0500

    back django blog

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+	creado = models.DateTimeField(auto_now_add=True, db_index=True)
+	titulo = models.CharField(blank=True,max_length=255)
+	img = models.ImageField(upload_to='media/')
+	contenido = models.TextField()
+
+	def __unicode__(self):
+		return self.nombre
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from blog.models import *
+
+def index_view(request):
+	posts= Post.objects.all()
+ 	return render_to_response('index.html',context_instance=RequestContext(request,{'posts': posts}))   
